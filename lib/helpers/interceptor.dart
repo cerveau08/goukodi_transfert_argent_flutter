0542eee5bd0e1eea9d531cb271787060263c8324
cerveau08 <malickcoly342@gmail.com>
2020-06-01 21:46:37 +0000

    login ok

---
+++ lib/helpers/interceptor.dart
@@ -0,0 +1,21 @@
+import 'package:http_interceptor/http_interceptor.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Interceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    var token=preferences.getString("token");
+    data.headers['Authorization'] =  'Bearer $token';
+    print(data.headers);
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print(data);
+    return data;
+  }
+
+}
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:goukodi_transfert_flutter/pages/home.dart';
+import 'package:jaguar_jwt/jaguar_jwt.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class Login extends StatefulWidget {
   @override
@@ -8,10 +15,145 @@ class Login extends StatefulWidget {
 }
 
 class _Login extends State<Login> {
+
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  String token;
+  String error;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GOUKODI')
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'GOUKODI',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 30
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: (error == null)?
+              Text(
+                "Connectez-vous",
+                style: GoogleFonts.openSans(
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600
+                  )
+                ),
+              ):
+              Text(error,
+                style: GoogleFonts.openSans(
+                  textStyle: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600
+                  )
+                )
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                obscureText: false,
+                controller: usernameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username'
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: RaisedButton(
+                textColor: Colors.white,
+                color: Colors.blue,
+                child: Text('Se Connecter'),
+                onPressed: () {
+                  login(usernameController.text, passwordController.text);
+                },
+              ),
+            )
+          ],
+        )
+      ),
     );
+  }
+  
+  void login(String username, String password) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    print(usernameController.text);
+    print(passwordController.text);
+    String url = "http://10.0.2.2:8000/api/login_check";
+    var body = {
+      "username": username,
+      "password": password
+    };
+    var reponse = await http.post(
+      url,
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: json.encode(body)
+    );
+    if(reponse.statusCode>=200 && reponse.statusCode < 400){
+      this.token = json.decode(reponse.body)['token'];
+      preferences.setString('token', this.token);
+
+      // decodage token
+      final String token = this.token;
+      final parts = token.split('.');
+      final payload = parts[1];
+      final String decoded = B64urlEncRfc7515.decodeUtf8(payload);
+
+      var tok = json.decode(decoded);
+      preferences.setString('role', tok["roles"][0]);
+      preferences.setInt('id', tok["id"]);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Home()
+        )
+      );
+    }else{
+      error = json.decode(reponse.body)['message'];
+      print(error);
+      if(error=="Invalid credentials."){
+      setState(() {
+        error="Identifiants Incorrects";
+      });
+      }else{
+        setState(() {
+          error=error;
+        });
+      }
+    }
   }
 }
